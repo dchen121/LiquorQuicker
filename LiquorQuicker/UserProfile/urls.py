dbbccabfa3386581ba71c395252228a087fee6cf
Michael Byrnes <JaNatuurlijk231@gmail.com>
2015-10-26 10:24:57 -0700

    Added test for the JQUser. Added an image to the profile template.

--- LiquorQuicker/UserProfile/urls.py
+++ LiquorQuicker/UserProfile/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     url(r'^$', views.ProfileView.as_view(), name='profile'),
     url(r'^login/$', views.Login.as_view(), name='login'),
     url(r'^login/auth_user/$', views.auth_user, name='auth_user'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
