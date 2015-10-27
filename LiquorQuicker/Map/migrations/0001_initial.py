8f4a99556631a542c21a4cb3fb3a91fe46307ce5
Edmund Wong <w.edmund.hh@gmail.com>
2015-10-27 12:06:01 -0700

    Added Location model to Maps and added .gitignore for .DS_Store and __pycache__.

---
+++ LiquorQuicker/Map/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Location',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('address', models.CharField(max_length=200)),
+                ('store_name', models.CharField(max_length=200)),
+            ],
+        ),
+    ]
--- LiquorQuicker/Map/models.py
+++ LiquorQuicker/Map/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+	address = models.CharField(max_length=200)
+	store_name = models.CharField(max_length=200)	
+
+	def __str__(self):
+		return self.store_name
+
