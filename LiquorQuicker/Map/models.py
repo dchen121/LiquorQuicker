f93ef6104cbe234ece5c8c98189e4453601c9b6e
Edmund Wong <w.edmund.hh@gmail.com>
2015-10-29 01:36:45 -0700

    Added latitude and longitude fields to Location model. Implemented method for geocoding using Google Maps Python library (pip install -U googlemaps). Markers appear on map but need more logic to handle locations better.

--- LiquorQuicker/Map/models.py
+++ LiquorQuicker/Map/models.py
@@ -5,9 +5,25 @@ class LiquorLocation(models.Model):
     store_name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
+	latitude = models.FloatField(null=True, blank=True)
+	longitude = models.FloatField(null=True, blank=True)	
 
     def __str__(self):
         return self.store_name
+
+    # Use Google Maps API Geocoding service to get the latitude/longitude for a certain address
+	def get_lat_long(self):
+		gmaps = Client(key=settings.GMAPS_API_KEY)
+		results = gmaps.geocode(self.address)
+		lat_lng = results[0]['geometry']['location']
+		return lat_lng
+
+	# Overriding save() to get latitude and longitude automatically instead of manually filling in
+	def save(self, *args, **kwargs):
+		lat_long = self.get_lat_long()
+		self.latitude = lat_long['lat']
+		self.longitude = lat_long['lng']
+		super(Location, self).save(*args, **kwargs)
 
 
 class PrivateStore(LiquorLocation):
--- LiquorQuicker/Map/views.py
+++ LiquorQuicker/Map/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
+from Map.models import Location
 
 
 class MapView(TemplateView):
@@ -8,3 +9,8 @@ class MapView(TemplateView):
     A view of the Map
     """
     template_name = 'Map/map.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(MapView, self).get_context_data(**kwargs)
+		context['locations_list'] = Location.objects.all()
+		return context
