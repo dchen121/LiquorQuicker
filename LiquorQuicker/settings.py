4a0969985dff0bae0a72bb0b0bd3276c59ef6935
Daniel Chen <Daniel@dhcp-206-87-153-186.ubcsecure.wireless.ubc.ca>
2015-11-02 18:27:46 -0800

    preparing to deploy to heroku

--- LiquorQuicker/settings.py
+++ LiquorQuicker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x!1k(-l0i4hdoh!!hr%j3w70s)r3lih%r8r*x@rj-$6)=%lw-y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
