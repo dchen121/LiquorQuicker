9646ae50d2905973236ebbd1fe365070d781d939
Richard Chiang <shadowlairs@gmail.com>
2015-11-02 19:36:06 -0800

    google verification

--- Map/urls.py
+++ Map/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.MapView.as_view(), name='map'),
+    url(r'^google-site-verification: google425a70456562b518.html$', TemplateView.as_view(template_name='google-verification.html')),
 ]
