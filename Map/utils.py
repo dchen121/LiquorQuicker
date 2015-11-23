Fixed distance formula for closest points function

Map/utils.py
@@ -1,21 +1,35 @@
 from .models import LiquorLocation
 import numpy as np
+import math
+
+# Adapted from https://gist.github.com/rochacbruno/2883505
+# Haversine formula example in Python
+# Author: Wayne Dyck
+
+def distance(origin, destination):
+    origin = np.radians(np.array(origin))
+    destination = np.radians(np.array(destination))
+    radius = 6371 # km
+
+    delta = np.radians(destination - origin)
+    a = np.square(np.sin(delta[:,0]/2)) + np.cos(np.radians(origin[0])) * np.cos(np.radians(origin[1])) * np.square(np.sin(delta[:,1]/2))
+    c = 2 * np.arcsin(np.sqrt(a)) 
+    d = radius * c
+
+    return d
 
 def calc_closest_points(lat, lng, locations):
-    my_location = np.array([lat, lng])
-    latLng = np.array(locations)
-    latLng[:,1:] = latLng[:,1:] - my_location
-    # getting the Euclidean distance from each point to our location
-    latLng[:,1] = np.linalg.norm(latLng[:,1:],2,1)
-    ind = np.argsort(latLng[:,1]).tolist()
+    dist = distance((lat, lng), locations)
+    ind = np.argsort(dist).tolist()
     return ind
 
 def get_closest_points(lat, lng, locations, count):
-    location_values = locations.values_list('id', 'latitude', 'longitude')
+    location_values = locations.values_list('latitude', 'longitude')
     ind = calc_closest_points(lat, lng, location_values)
     sorted_locations = []
-    print(locations)
     for i in range(0, count):
         sorted_locations.append(locations[ind[i]])
 
     return sorted_locations
+
+